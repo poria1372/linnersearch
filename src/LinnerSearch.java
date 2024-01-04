c5e8a0c06d32556cf16e87f4b9c3ac48f40eb7a4
poria.1372 <poria.karimi1372@gmail.com>
2024-01-04 20:51:57 +0330

    linnerSearchTestClass

--- src/LinnerSearch.java
+++ src/LinnerSearch.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class LinnerSearch {
+public class LinnerSearch {
+
+    public static int linnerSearchTest(int[] array, int number) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == number) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Main {
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        SecureRandom secureRandom = new SecureRandom();
+        int [] array = new int[10];
+        for (int i = 0 ; i<array.length; i++){
+            array[i] = secureRandom.nextInt(90);
+        }
+        System.out.printf("%s%n%n", Arrays.toString(array));
+
+        System.out.printf("please enter a number  :");
+        int number  = input.nextInt();
+        while (number != -1){
+            int returnIndex = LinnerSearch.linnerSearchTest(array,number);
+            if (returnIndex == -1){
+                System.out.printf("%d was not found %n%n",number);
+            }else {
+                System.out.printf("%d was fount in position %d%n%n",number,returnIndex);
+            }
+            System.out.printf("enter -1 quit");
+            number = input.nextInt();
+        }
+    }
+
 }
